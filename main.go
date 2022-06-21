e3bf9bdc942af4e58a4355a0ffebed8a2bc2c0e1
Bruno Habermann <habermann.bruno@gmail.com>
2022-06-20 21:37:34 -0300

    Add load .env func

--- main.go
+++ main.go
@@ -3,7 +3,16 @@ package main
 import (
 	"fmt" 
 	myroute "github.com/bhabermann/imersaofc8-simulator/app/route"
+	"github.com/joho/godotenv"
+	"log"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 	route := myroute.Route{

eae6a8912241bf32fdb677a110f1405350c89238
Bruno Habermann <habermann.bruno@gmail.com>
2022-06-20 21:29:57 -0300

    Add consumer

---
+++ infra/kafka/consumer.go
@@ -0,0 +1,31 @@
+package kafka
+
+import(
+	"fmt"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
+	"os"
+) 
+
+type KafkaConsumer struct {
+	MsgChan chan *ckafka.Message
+}
+
+func(k *KafkaConsumer) Consume() {
+	configMap := ckafka.ConfigMap{
+		"bootstrap.servers": os.Getenv("KafkaBootstrapServers"),
+		"group.id":          os.Getenv("KafkaConsumerGroupID"),
+	}
+	c, err := ckafka.NewConsumer(configMap)
+	if err != nil {
+		log.Fatal("Error consuming kafka message: " + err.Error())
+	}
+	topics := []string{os.Getenv("KafkaReadTopic")}
+	fmt.Println("Kafka consumer has been starter")
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			k.MsgChan <- msg
+		}
+	}
+}

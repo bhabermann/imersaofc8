ab5ce4cfda5b3acbbcb72e8122339c357e2abd71
Bruno Habermann <habermann.bruno@gmail.com>
2022-06-22 21:47:48 -0300

    Test consumer

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/bhabermann/imersaofc8-simulator/infra/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/joho/godotenv"
 )
 
@@ -15,12 +17,21 @@ func init() {
 }
 
 func main() {
-	producer := kafka.NewKafkaProducer()
-	kafka.Publish("Hello World", "route.new-direction", producer)
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
 
-	for {
-		_ = 1
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
 	}
+
+	// producer := kafka.NewKafkaProducer()
+	// kafka.Publish("Hello World", "route.new-direction", producer)
+
+	// for {
+	// 	_ = 1
+	// }
+
 	// route := myroute.Route{
 	// 	ID:       "1",
 	// 	ClientID: "1",
